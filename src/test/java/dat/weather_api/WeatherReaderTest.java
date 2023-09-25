3244b52b8a51fc12bf3e267f13a52f99fe81c655
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-09-25 13:25:00 +0200

    Create WeatherReaderTest.java

---
+++ src/test/java/dat/weather_api/WeatherReaderTest.java
@@ -0,0 +1,44 @@
+package dat.weather_api;
+
+import dat.data.dto.weather.CurrentDataDTO;
+import dat.data.dto.weather.WeatherReaderDTO;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeatherReaderTest
+{
+
+    private WeatherReader weatherReader;
+    @BeforeEach
+    void setUp()
+    {
+        weatherReader = new WeatherReader();
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+    }
+
+    @Test
+    void read()
+    {
+        WeatherReaderDTO weatherReaderDTO = weatherReader.read("København");
+        assertNotNull(weatherReaderDTO);
+
+        WeatherReaderDTO wrd = WeatherReaderDTO.builder()
+                .LocationName("København, Region Hovedstaden")
+                .CurrentData(CurrentDataDTO.builder()
+                        .temperature("17")
+                        .skyText("Skyet")
+                        .humidity("78")
+                        .windText("19 m/s Syd")
+                        .build())
+                .build();
+
+        assertEquals(wrd.toString(), weatherReaderDTO.toString());
+    }
+}
