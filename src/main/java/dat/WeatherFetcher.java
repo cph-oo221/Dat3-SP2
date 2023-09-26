6cf7557ec1f95f93d5552982c4661509d96f2c55
kotteletfisk <ostekugle@gmail.com>
2023-09-26 11:58:22 +0200

    Can now fetch weatherdata concurrently

--- src/main/java/dat/WeatherFetcher.java
+++ src/main/java/dat/WeatherFetcher.java
@@ -6,21 +6,27 @@ import dat.model.City;
 import dat.model.Weather;
 
 import java.io.IOException;
+import java.util.concurrent.Callable;
 
-public class WeatherHandler
+public class WeatherFetcher implements Callable<Weather>
 {
+
     String ApiLocation;
     City city;
 
-    public WeatherHandler(City city)
+    public WeatherFetcher(City city)
     {
         this.city = city;
         this.ApiLocation = city.getName() + ", " + city.getCountry();
     }
 
+    @Override
+    public Weather call() throws IOException
+    {
+        return getWeatherEntity();
+    }
 
-
-    public Weather getWeatherEntity() throws IOException
+    private Weather getWeatherEntity() throws IOException
     {
         WeatherReader weatherReader = new WeatherReader();
         WeatherWebscraper weatherWebscraper = new WeatherWebscraper(city.getYrUrl());
@@ -30,6 +36,4 @@ public class WeatherHandler
 
         return w;
     }
-
-
 }
